39c554902b9e4c5f98c59e9fe25eda514c1835d9
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-10-18 21:00:53 -0700

    Removed unused variables

--- python/deepcontroller.py
+++ python/deepcontroller.py
@@ -2,7 +2,7 @@ import opensim as osim
 
 import sys
 
-# -- CONFIG --
+# Some meta parameters
 stepsize = 0.05
 nsteps = 100
 
@@ -12,7 +12,7 @@ model = osim.Model("../models/gait9dof18musc_Thelen_BigSpheres_20161017.osim")
 # Enable the visualizer
 model.setUseVisualizer(True)
 
-# Construct the controller
+# Get the muscles
 muscleSet = model.getMuscles()
 
 # Initialize simulation
@@ -20,17 +20,14 @@ state = model.initSystem()
 model.equilibrateMuscles(state)
 manager = osim.Manager(model)
 
-actControls = osim.Vector(1, 0.0);
-time = osim.Vector(1, state.getTime());
-
-actControls[0] = 1
-
 # Simulatie
 for i in range(0,nsteps):
     # activate the muscles corresponding to the current step
     # yes, this makes no sense, it's just a test
     muscle = muscleSet.get(i % 18)
     muscle.setActivation(state, 20)
+
+    # Integrate one step
     manager.setInitialTime(stepsize * i)
     manager.setFinalTime(stepsize * (i + 1))
     manager.integrate(state)
