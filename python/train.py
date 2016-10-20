51ee3f14f01a98612d69491d93f578e82ccfaca8
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-10-20 14:50:18 -0700

    Some attempts to integrate keras

--- python/train.py
+++ python/train.py
@@ -1,16 +1,20 @@
 import opensim as osim
 import numpy as np
 import sys
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
 
 # Some meta parameters
 stepsize = 0.05
-nsteps = 100
+nsteps = 10
+ninput = 4
+noutput = 18
 
 # Get the model
 model = osim.Model("../models/gait9dof18musc_Thelen_BigSpheres_20161017.osim")
 
 # Enable the visualizer
-model.setUseVisualizer(True)
+model.setUseVisualizer(False)
 
 # Get the muscles
 muscleSet = model.getMuscles()
@@ -30,13 +34,37 @@ for i in xrange(13):
 for i in xrange(13):
     print(jointSet.get(i).getName())
 
-head = jointSet.get(0)
-head = osim.PlanarJoint.safeDownCast(head)
+ground_pelvis = osim.PlanarJoint.safeDownCast(jointSet.get(0))
+
+hip_r = osim.PinJoint.safeDownCast(jointSet.get(1))
+knee_r = osim.CustomJoint.safeDownCast(jointSet.get(2))
+ankle_r = osim.PinJoint.safeDownCast(jointSet.get(3))
+subtalar_r = osim.WeldJoint.safeDownCast(jointSet.get(4))
+mtp_r = osim.WeldJoint.safeDownCast(jointSet.get(5))
+
+hip_l = osim.PinJoint.safeDownCast(jointSet.get(6))
+knee_l = osim.CustomJoint.safeDownCast(jointSet.get(7))
+ankle_l = osim.PinJoint.safeDownCast(jointSet.get(8))
+subtalar_l = osim.WeldJoint.safeDownCast(jointSet.get(9))
+mtp_l = osim.PinJoint.safeDownCast(jointSet.get(10))
+
+back = osim.PinJoint.safeDownCast(jointSet.get(11))
+back1 = osim.WeldJoint.safeDownCast(jointSet.get(12))
+
+# DEFINE THE BRAIN
+net = Sequential()
+net.add(Dense(output_dim=noutput, input_dim=ninput))
+net.add(Activation("relu"))
+net.add(Dense(output_dim=noutput))
+net.add(Activation("softmax"))
+net.compile(loss='mean_squared_error', optimizer='sgd')
 
 def brain_model(invars):
     # activations = F(params, invars)
-    activations = np.array([0] * 18)
-    return activations
+    # THE NEURAL NET COMES HERE!
+    activations = np.array([0] * noutput)
+    activations = net.predict(invars.reshape(-1,4))
+    return activations[0]
 
 class Objective:
     sumOfObjectives = 0
@@ -48,7 +76,9 @@ class Objective:
         self.sumOfObjectives = 0
     
     def update(self, state):
-        self.sumOfObjectives = self.sumOfObjectives - head.getCoordinate(1).getValue(state)
+        upd = - ground_pelvis.getCoordinate(1).getValue(state)
+        self.sumOfObjectives = self.sumOfObjectives + upd
+        return upd
 
     def value(self):
         return self.sumOfObjectives
@@ -57,16 +87,20 @@ obj = Objective()
     
 # Simulatie
 for i in range(0,nsteps):
-    invars = np.array([0] * (26 * 3))
+    invars = np.array([0] * ninput)
 
-#    for j in xrange(26):
-#        print(forceSet.get(j))
+    # for j in range(18):
+    #     invars[j] = muscleSet.get(j).getExcitation(state)
+    invars[0] = ground_pelvis.getCoordinate(1).getValue(state)
+    invars[1] = ground_pelvis.getCoordinate(2).getValue(state)
+    invars[2] = hip_r.getCoordinate(0).getValue(state)
+    invars[3] = hip_l.getCoordinate(0).getValue(state)
     
     activations = brain_model(invars)
     
-    for j in range(18):
-        muscle = muscleSet.get(j % 18)
-        muscle.setActivation(state, activations[j])
+    for j in range(noutput):
+        muscle = muscleSet.get(j)
+        muscle.setActivation(state, activations[j] * 10)
 
     obj.update(state)
 
@@ -74,3 +108,7 @@ for i in range(0,nsteps):
     manager.setInitialTime(stepsize * i)
     manager.setFinalTime(stepsize * (i + 1))
     manager.integrate(state)
+
+    net.train_on_batch(np.array([invars]), np.array([[1]]))
+
+print(obj.value())
