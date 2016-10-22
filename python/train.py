10a0e07937d53335ed992d21a54551af6ddff14d
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-10-21 20:43:59 -0700

    Adapted to keras-rl framework

--- python/train.py
+++ python/train.py
@@ -1,35 +1,24 @@
 import opensim as osim
 import numpy as np
 import sys
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Flatten, Input, merge
+from keras.optimizers import Adam
+
+import numpy as np
+
+from rl.agents import ContinuousDQNAgent
+from rl.memory import SequentialMemory
+from rl.random import OrnsteinUhlenbeckProcess
 
 # Some meta parameters
 stepsize = 0.05
-nsteps = 10
+nallsteps = 50000
 ninput = 4
 noutput = 18
 visualize = False
-
-class Policy:
-    net = Sequential()
-
-    def __init__(self):
-        # DEFINE THE BRAIN
-        self.net.add(Dense(output_dim=noutput, input_dim=ninput))
-        self.net.add(Activation("relu"))
-        self.net.add(Dense(output_dim=noutput))
-        self.net.add(Activation("softmax"))
-        self.net.compile(loss='mean_squared_error', optimizer='sgd')
-
-    def get_action(self, state):
-        # activations = F(params, invars)
-        # THE NEURAL NET COMES HERE!
-        self.activations = np.array([0] * noutput)
-        self.activations = self.net.predict(state.reshape(-1,4))
-        return self.activations[0]
-
-    def get_params(self)
+nb_actions = noutput
+nepisodesteps = 50
 
 class Objective:
     rewards = []
@@ -41,14 +30,14 @@ class Objective:
         self.sum_rewards = 0
     
     def update(self, agent):
-        reward = - agent.ground_pelvis.getCoordinate(1).getValue(agent.state)
+        reward = agent.ground_pelvis.getCoordinate(1).getValue(agent.state)
         self.sum_rewards = self.sum_rewards + reward
         return reward
 
     def reward(self):
         return self.sum_rewards
 
-class Agent:
+class Environment:
     # Initialize simulation
     model = None
     state = None
@@ -56,7 +45,7 @@ class Agent:
 
     obj = Objective()
 
-    step = 0
+    istep = 0
     
     def __init__(self):
         # Get the model
@@ -97,9 +86,8 @@ class Agent:
 
         self.reset()
 
-
     def reset(self):
-        self.step = 0
+        self.istep = 0
         if not self.state0:
             self.state0 = self.model.initSystem()
         self.state = osim.State(self.state0)
@@ -107,44 +95,98 @@ class Agent:
         self.model.equilibrateMuscles(self.state)
         self.manager = osim.Manager(self.model)
         self.obj.reset()
+        return self.get_observation()
 
-
-    def simulate_step(self, policy):
+    def get_observation(self):
         invars = np.array([0] * ninput)
 
         invars[0] = self.ground_pelvis.getCoordinate(1).getValue(self.state)
         invars[1] = self.ground_pelvis.getCoordinate(2).getValue(self.state)
         invars[2] = self.hip_r.getCoordinate(0).getValue(self.state)
         invars[3] = self.hip_l.getCoordinate(0).getValue(self.state)
-    
-        activations = policy.get_action(invars)
-    
+        return invars
+
+    def step(self, action):
         for j in range(noutput):
             muscle = self.muscleSet.get(j)
-            muscle.setActivation(self.state, activations[j] * 10)
+            muscle.setActivation(self.state, action[j] * 10)
 
         reward = self.obj.update(self)
 
         # Integrate one step
-        self.manager.setInitialTime(stepsize * self.step)
-        self.manager.setFinalTime(stepsize * (self.step + 1))
+        self.manager.setInitialTime(stepsize * self.istep)
+        self.manager.setFinalTime(stepsize * (self.istep + 1))
         self.manager.integrate(self.state)
 
-        self.step = self.step + 1
+        self.istep = self.istep + 1
 
-        return reward
+        return self.get_observation(), reward, self.is_done(), False
 
-    def simulate_episode(self, policy):
-        self.reset()
-        for i in range(0,nsteps):
-            self.simulate_step(policy)
-        return self.obj.reward()
+    def is_done(self):
+        return self.istep >= nepisodesteps
 
-policy = Policy()
-agent = Agent()
-    
-print(agent.simulate_episode(policy))
-print(agent.simulate_episode(policy))
-print(agent.simulate_episode(policy))
-print(agent.simulate_episode(policy))
-print(agent.simulate_episode(policy))
+    def render(self, *args, **kwargs):
+        return
+
+# Build all necessary models: V, mu, and L networks.
+V_model = Sequential()
+V_model.add(Flatten(input_shape=(1,) + (ninput, )))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(1))
+V_model.add(Activation('linear'))
+print(V_model.summary())
+
+mu_model = Sequential()
+mu_model.add(Flatten(input_shape=(1,) + (ninput, )))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(nb_actions))
+mu_model.add(Activation('linear'))
+print(mu_model.summary())
+
+action_input = Input(shape=(nb_actions,), name='action_input')
+observation_input = Input(shape=(1,) + (ninput, ), name='observation_input')
+x = merge([action_input, Flatten()(observation_input)], mode='concat')
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(((nb_actions * nb_actions + nb_actions) / 2))(x)
+x = Activation('linear')(x)
+L_model = Model(input=[action_input, observation_input], output=x)
+print(L_model.summary())
+
+env = Environment()
+
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+memory = SequentialMemory(limit=100000, window_length=1)
+random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.3, size=nb_actions)
+agent = ContinuousDQNAgent(nb_actions=nb_actions, V_model=V_model, L_model=L_model, mu_model=mu_model,
+                           memory=memory, nb_steps_warmup=100, random_process=random_process,
+                           gamma=.99, target_model_update=1e-3)
+agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+agent.fit(env, nb_steps=nallsteps, visualize=True, verbose=1, nb_max_episode_steps=nepisodesteps)
+
+# After training is done, we save the final weights.
+agent.save_weights('cdqn_weights.h5f', overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+agent.test(env, nb_episodes=10, visualize=True, nb_max_episode_steps=200)
+
+
