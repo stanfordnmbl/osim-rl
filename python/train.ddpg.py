03a3d9a4a9ccb6fb54efd43486c79c1530f522ad
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-11-02 00:54:39 -0500

    Details...

--- python/train.ddpg.py
+++ python/train.ddpg.py
@@ -72,7 +72,7 @@ print(critic.summary())
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
 memory = SequentialMemory(limit=100000, window_length=1)
-random_process = OrnsteinUhlenbeckProcess(theta=args.theta, mu=0., sigma=args.sigma, size=env.noutput)
+random_process = OrnsteinUhlenbeckProcess(theta=float(args.theta), mu=0., sigma=float(args.sigma), size=env.noutput)
 agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
                   memory=memory, nb_steps_warmup_critic=100, nb_steps_warmup_actor=100,
                   random_process=random_process, gamma=.99, target_model_update=1e-3,
