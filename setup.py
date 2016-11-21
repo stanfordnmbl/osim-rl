e37f04032476cd9c429f53bd5b2cbe9eb73bcd79
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-11-21 15:21:08 -0800

    pip installable

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+# This provides the variable `__version__`.
+# execfile('opensim/version.py')
+__version__ = 1.1
+
+setup(name='osim-rl',
+      version=__version__,
+      description='OpenSim Reinforcement Learning Framework',
+      author='Lukasz Kidzinski',
+      author_email='lukasz.kidzinski@stanford.edu',
+      url='http://opensim.stanford.edu/',
+      license='Apache 2.0',
+      packages=['osim'],
+      package_data={'osim': ['models/*']},
+      include_package_data=True,
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          ],
+      )
