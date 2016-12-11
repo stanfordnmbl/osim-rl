c0858a289c7cb893d097dca720582b684b43c103
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-12-11 09:26:31 -0800

    CDQN training

---
+++ scripts/keras-rl/train.cdqn.py
@@ -0,0 +1,100 @@
+import opensim as osim
+import numpy as np
+import gym
+
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Flatten, Input, merge
+from keras.optimizers import Adam
+
+from rl.agents import ContinuousDQNAgent
+from rl.memory import SequentialMemory
+from rl.random import OrnsteinUhlenbeckProcess
+from rl.core import Processor
+
+from osim.env import ArmEnv
+
+import argparse
+
+ENV_NAME = 'Pendulum-v0'
+gym.undo_logger_setup()
+
+# Command line parameters
+parser = argparse.ArgumentParser(description='Train or test neural net motor controller')
+parser.add_argument('--train', dest='train', action='store_true', default=True)
+parser.add_argument('--test', dest='train', action='store_false', default=True)
+parser.add_argument('--visualize', dest='visualize', action='store_true', default=False)
+parser.add_argument('--output', dest='output', action='store', default="model.h5f")
+parser.add_argument('--env', dest='env', action='store', default="Arm")
+args = parser.parse_args()
+
+if args.env == "Gait":
+    env = GaitEnv(args.visualize)
+else:
+    env = ArmEnv(args.visualize)
+
+nb_actions = env.action_space.shape[0]
+
+# Build all necessary models: V, mu, and L networks.
+V_model = Sequential()
+V_model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(16))
+V_model.add(Activation('relu'))
+V_model.add(Dense(1))
+V_model.add(Activation('linear'))
+print(V_model.summary())
+
+mu_model = Sequential()
+mu_model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(16))
+mu_model.add(Activation('relu'))
+mu_model.add(Dense(nb_actions))
+mu_model.add(Activation('linear'))
+print(mu_model.summary())
+
+action_input = Input(shape=(nb_actions,), name='action_input')
+observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
+x = merge([action_input, Flatten()(observation_input)], mode='concat')
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(32)(x)
+x = Activation('relu')(x)
+x = Dense(((nb_actions * nb_actions + nb_actions) / 2))(x)
+x = Activation('linear')(x)
+L_model = Model(input=[action_input, observation_input], output=x)
+print(L_model.summary())
+
+class RewardProcessor(Processor):
+    def process_reward(self, reward):
+        # No modification for the moment
+        return reward
+
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+processor = RewardProcessor()
+memory = SequentialMemory(limit=100000, window_length=1)
+random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.3, size=nb_actions)
+agent = ContinuousDQNAgent(nb_actions=nb_actions, V_model=V_model, L_model=L_model, mu_model=mu_model,
+                           memory=memory, nb_steps_warmup=100, random_process=random_process,
+                           gamma=.99, target_model_update=1e-3, processor=processor)
+agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+agent.fit(env, nb_steps=100000, visualize=True, verbose=1, nb_max_episode_steps=200)
+
+# After training is done, we save the final weights.
+agent.save_weights('cdqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+agent.test(env, nb_episodes=10, visualize=True, nb_max_episode_steps=200)
