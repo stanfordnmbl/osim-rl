54a685f2511581a9a2271401f831db1975dd3e49
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-02-21 10:19:06 -0800

    Token is now required

--- scripts/submit.py
+++ scripts/submit.py
@@ -7,13 +7,12 @@ import numpy as np
 import argparse
 
 # Settings
-CROWDAI_TOKEN = "518ec33d7af656bddfcb83ab614ba079"
 remote_base = 'http://grader.crowdai.org'
 
 # Command line parameters
 parser = argparse.ArgumentParser(description='Submit the result to crowdAI')
 parser.add_argument('--model', dest='model', action='store', default="example_actor.h5f")
-parser.add_argument('--token', dest='token', action='store', default=CROWDAI_TOKEN)
+parser.add_argument('--token', dest='token', action='store', required=True)
 args = parser.parse_args()
 
 env = GaitEnv(visualize=False)
