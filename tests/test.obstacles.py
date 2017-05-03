448322fae035d1ab73e1b91d63c1d9f5c9d021ee
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-05-02 17:16:47 -0700

    Test obstacles

---
+++ tests/test.obstacles.py
@@ -0,0 +1,54 @@
+# This script goes through OpenSim funcionalties
+# required for OpenSim-RL
+import opensim
+
+# Settings
+stepsize = 0.01
+
+# Load existing model
+model_path = "../osim/models/gait9dof18musc.osim"
+model = opensim.Model(model_path)
+model.setUseVisualizer(True)
+
+# Create the ball
+r = 0.1
+ballBody = opensim.Body('ball', 0.0001 , opensim.Vec3(0), opensim.Inertia(1,1,.0001,0,0,0) );
+ballGeometry = opensim.Ellipsoid(r, r, r)
+ballGeometry.setColor(opensim.Gray)
+ballBody.attachGeometry(ballGeometry)
+
+# Attach ball to the model
+ballJoint = opensim.WeldJoint("weldball",
+                         model.getGround(), # PhysicalFrame
+                         opensim.Vec3(0, 0, 0),
+                         opensim.Vec3(0, 0, 0),
+                         ballBody, # PhysicalFrame
+                         opensim.Vec3(0, 0, 0),
+                         opensim.Vec3(0, 0, 0))
+model.addComponent(ballJoint)
+model.addComponent(ballBody)
+
+# Add contact
+ballContact = opensim.ContactSphere(r, opensim.Vec3(0,0,0), ballBody);
+model.addContactGeometry(ballContact)
+
+# Reinitialize the system with the new controller
+state = model.initSystem()
+
+# Simulate
+for i in range(100):
+    t = state.getTime()
+    manager = opensim.Manager(model)
+    manager.integrate(state, t + stepsize)
+
+    # Restart the model every 10 frames, with the new position of the ball
+    if (i + 1) % 10 == 0:
+        newloc = opensim.Vec3(float(i) / 10, 0, 0)
+        opensim.PhysicalOffsetFrame.safeDownCast(ballJoint.getChildFrame()).set_translation(newloc)
+
+        r = i * 0.01
+        ballGeometry.setEllipsoidParams(r,r,r)
+        ballContact.setRadius(r)
+
+        state = model.initializeState()
+        
