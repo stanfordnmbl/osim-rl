308bde653c94e7fa86e29963783605973029e28a
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-05-07 21:41:38 -0700

    Explicit difficulty

--- scripts/basic.py
+++ scripts/basic.py
@@ -2,8 +2,8 @@ from osim.env.run import RunEnv, generate_env
 
 env = RunEnv(visualize=True)
 
-observation = env.reset(10)
+observation = env.reset(difficulty=10)
 for i in range(500):
     observation, reward, done, info = env.step(env.action_space.sample())
     if i % 100 == 99 or done:
-        observation = env.reset(10)
+        observation = env.reset(difficulty=10)
