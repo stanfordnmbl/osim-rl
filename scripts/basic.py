164afb44609f9669ce74d6011ed0e824dcc95e63
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-12-20 23:11:21 -0800

    Activations in the observation vector

--- scripts/basic.py
+++ scripts/basic.py
@@ -1,12 +1,9 @@
 from osim.env.run import RunEnv
 
-env = RunEnv(visualize=True, max_obstacles = 10)
+env = RunEnv(visualize=True, max_obstacles = 0)
 
 observation = env.reset()
 for i in range(200):
     observation, reward, done, info = env.step(env.action_space.sample())
     if done:
         env.reset()
-#        break
-    
-
