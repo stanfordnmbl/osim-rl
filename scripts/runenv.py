3c86fa860d7d765b5fb0917e499502f899db8984
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2018-03-25 13:45:29 -0700

    Cleanup in scripts directory

--- scripts/runenv.py
+++ scripts/runenv.py
@@ -5,9 +5,7 @@ env = L2RunEnv(visualize=True)
 
 observation = env.reset()
 for i in range(200):
-    res = env.step(env.action_space.sample())
-    observation, reward, done, info = res
-    print(res)
+    observation, reward, done, info = env.step(env.action_space.sample())
 
     # pprint.pprint(env.get_state_desc())
     if done:
