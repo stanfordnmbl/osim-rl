6336c3c9c3c0376439c4998d5920db27859a15cb
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2018-03-25 14:31:30 -0700

    Deleting unused code from a few generations ago

--- osim/sandbox/test.py
+++
@@ -1,7 +0,0 @@
-from environments.arm import ArmEnv
-
-env = ArmEnv(visualize=True)
-env.reset()
-
-for i in range(10000):
-    env.step([i] * 6)
--- osim/sandbox/train.py
+++
@@ -1,7 +0,0 @@
-from environments.arm import ArmEnv
-
-env = ArmEnv()
-env.reset()
-
-for i in xrange(100):
-    env.step()
