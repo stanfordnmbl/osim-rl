d29d8b5831e635a1e8daadd314fae1d235d2928b
SP Mohanty <spmohanty91@gmail.com>
2018-06-11 18:51:21 +0200

    Update submit example script

--- examples/submit.py
+++ examples/submit.py
@@ -1,13 +1,12 @@
 import opensim as osim
 from osim.http.client import Client
-from osim.env import *
+from osim.env import ProstheticsEnv
 import numpy as np
 import argparse
 from pprint import pprint
 
 # Settings
 remote_base = 'http://grader.crowdai.org:1729'
-remote_base = 'http://0.0.0.0:5000'
 
 # Command line parameters
 parser = argparse.ArgumentParser(description='Submit the result to crowdAI')
@@ -17,17 +16,15 @@ args = parser.parse_args()
 client = Client(remote_base)
 
 # Create environment
-observation = client.env_create(args.token)
+observation = client.env_create(args.token, env_id="ProstheticsEnv")
 env = ProstheticsEnv()
 
 # Run a single step
 #
 # The grader runs 3 simulations of at most 1000 steps each. We stop after the last one
 while True:
-    pprint(observation)
-#    v = np.array(observation).reshape((-1,1,env.observation_space.shape[0]))
-    [observation, reward, done, info] = client.env_step(env.action_space.sample().tolist())
     print(observation)
+    [observation, reward, done, info] = client.env_step(env.action_space.sample().tolist())
     if done:
         observation = client.env_reset()
         if not observation:
