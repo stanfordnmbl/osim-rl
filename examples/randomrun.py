514260fcb36fb403fc976e6805714f5ccb3c17e1
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2018-06-29 11:24:14 -0700

    Run3d moved to examples (renamed to randomrun.py)

--- examples/randomrun.py
+++ examples/randomrun.py
@@ -1,13 +1,9 @@
 from osim.env import ProstheticsEnv
-import pprint
 
 env = ProstheticsEnv()
 env.change_model(model='3D', prosthetic=True, difficulty=2, seed=None)
 observation = env.reset()
 for i in range(300):
     observation, reward, done, info = env.step(env.action_space.sample(), project = False)
-#    print(len(observation))
-#    pprint.pprint(observation)
-    print(len(env.action_space.sample()))
     if done:
         env.reset()
