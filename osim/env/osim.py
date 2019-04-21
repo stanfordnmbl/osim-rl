a6210d2ccfd1997ab8e28dc69e284460a1b5e306
smsong <ssm0445@gmail.com>
2019-04-21 12:15:14 -0700

    [L2M2019] started

--- osim/env/osim.py
+++ osim/env/osim.py
@@ -571,6 +571,162 @@ class ProstheticsEnv(OsimEnv):
         return self.reward_round2()
 
 
+class L2M2019Env(OsimEnv):
+    model = "3D"
+    def get_model_key(self):
+        return self.model
+
+    def set_difficulty(self, difficulty):
+        self.difficulty = difficulty
+        if difficulty == 0:
+            self.time_limit = 1000
+        if difficulty == 1:
+            self.time_limit = 1000
+        self.spec.timestep_limit = self.time_limit    
+
+    def __init__(self, visualize = True, integrator_accuracy = 5e-5, difficulty=0, seed=0, report=None):
+        self.model_paths = {}
+        self.model_paths["3D"] = os.path.join(os.path.dirname(__file__), '../models/gait14dof22musc_20170320.osim')
+        self.model_paths["2D"] = os.path.join(os.path.dirname(__file__), '../models/gait14dof22musc_planar_20170320.osim')
+        self.model_path = self.model_paths[self.get_model_key()]
+        super(L2M2019Env, self).__init__(visualize = visualize, integrator_accuracy = integrator_accuracy)
+        self.set_difficulty(difficulty)
+        random.seed(seed)
+        np.random.seed(seed)
+
+        if report:
+            bufsize = 0
+            self.observations_file = open("%s-obs.csv" % (report,),"w", bufsize)
+            self.actions_file = open("%s-act.csv" % (report,),"w", bufsize)
+            self.get_headers()
+
+    def change_model(self, model='3D', difficulty=0, seed=0):
+        if self.model != model:
+            self.model = model
+            self.load_model(self.model_paths[self.get_model_key()])
+        self.set_difficulty(difficulty)
+        random.seed(seed)
+        np.random.seed(seed)
+    
+    def is_done(self):
+        state_desc = self.get_state_desc()
+        return state_desc["body_pos"]["pelvis"][1] < 0.6
+
+    ## Values in the observation vector
+    # y, vx, vy, ax, ay, rz, vrz, arz of pelvis (8 values)
+    # x, y, vx, vy, ax, ay, rz, vrz, arz of head, torso, toes_l, toes_r, talus_l, talus_r (9*6 values)
+    # rz, vrz, arz of ankle_l, ankle_r, back, hip_l, hip_r, knee_l, knee_r (7*3 values)
+    # activation, fiber_len, fiber_vel for all muscles (3*18)
+    # x, y, vx, vy, ax, ay ofg center of mass (6)
+    # 8 + 9*6 + 8*3 + 3*18 + 6 = 146
+    def get_observation(self):
+        state_desc = self.get_state_desc()
+
+        # Augmented environment from the L2R challenge
+        res = []
+        pelvis = None
+
+        for body_part in ["pelvis", "head","torso","toes_l","toes_r","talus_l","talus_r"]:
+            cur = []
+            cur += state_desc["body_pos"][body_part][0:2]
+            cur += state_desc["body_vel"][body_part][0:2]
+            cur += state_desc["body_acc"][body_part][0:2]
+            cur += state_desc["body_pos_rot"][body_part][2:]
+            cur += state_desc["body_vel_rot"][body_part][2:]
+            cur += state_desc["body_acc_rot"][body_part][2:]
+            if body_part == "pelvis":
+                pelvis = cur
+                res += cur[1:]
+            else:
+                cur_upd = cur
+                cur_upd[:2] = [cur[i] - pelvis[i] for i in range(2)]
+                cur_upd[6:7] = [cur[i] - pelvis[i] for i in range(6,7)]
+                res += cur
+
+        for joint in ["ankle_l","ankle_r","back","hip_l","hip_r","knee_l","knee_r"]:
+            res += state_desc["joint_pos"][joint]
+            res += state_desc["joint_vel"][joint]
+            res += state_desc["joint_acc"][joint]
+
+        for muscle in sorted(state_desc["muscles"].keys()):
+            res += [state_desc["muscles"][muscle]["activation"]]
+            res += [state_desc["muscles"][muscle]["fiber_length"]]
+            res += [state_desc["muscles"][muscle]["fiber_velocity"]]
+
+        cm_pos = [state_desc["misc"]["mass_center_pos"][i] - pelvis[i] for i in range(2)]
+        res = res + cm_pos + state_desc["misc"]["mass_center_vel"] + state_desc["misc"]["mass_center_acc"]
+
+        return res
+
+    def get_observation_space_size(self):
+        return 169
+
+    def generate_new_targets(self, poisson_lambda = 300):
+        nsteps = self.time_limit + 1
+        rg = np.array(range(nsteps))
+        velocity = np.zeros(nsteps)
+        heading = np.zeros(nsteps)
+
+        velocity[0] = 1.25
+        heading[0] = 0
+
+        change = np.cumsum(np.random.poisson(poisson_lambda, 10))
+
+        for i in range(1,nsteps):
+            velocity[i] = velocity[i-1]
+            heading[i] = heading[i-1]
+
+            if i in change:
+                velocity[i] += random.choice([-1,1]) * random.uniform(-0.5,0.5)
+                heading[i] += random.choice([-1,1]) * random.uniform(-math.pi/8,math.pi/8)
+
+        trajectory_polar = np.vstack((velocity,heading)).transpose()
+        self.targets = np.apply_along_axis(rect, 1, trajectory_polar)
+        
+    def get_state_desc(self):
+        d = super(L2M2019Env, self).get_state_desc()
+        if self.difficulty > 0:
+            d["target_vel"] = self.targets[self.osim_model.istep,:].tolist()
+        return d
+
+    def reset(self, project = True, seed = None):
+        if seed:
+            random.seed(seed)
+            np.random.seed(seed)
+        self.generate_new_targets()
+        return super(L2M2019Env, self).reset(project = project)
+
+    def reward_round1(self):
+        state_desc = self.get_state_desc()
+        prev_state_desc = self.get_prev_state_desc()
+        if not prev_state_desc:
+            return 0
+        return 9.0 - (state_desc["body_vel"]["pelvis"][0] - 3.0)**2
+
+    def reward_round2(self):
+        state_desc = self.get_state_desc()
+        prev_state_desc = self.get_prev_state_desc()
+        penalty = 0
+
+        # Small penalty for too much activation (cost of transport)
+        penalty += np.sum(np.array(self.osim_model.get_activations())**2) * 0.001
+
+        # Big penalty for not matching the vector on the X,Z projection.
+        # No penalty for the vertical axis
+        penalty += (state_desc["body_vel"]["pelvis"][0] - state_desc["target_vel"][0])**2
+        penalty += (state_desc["body_vel"]["pelvis"][2] - state_desc["target_vel"][2])**2
+        
+        # Reward for not falling
+        reward = 10.0
+        
+        return reward - penalty 
+
+    def reward(self):
+        if self.difficulty == 0:
+            return self.reward_round1()
+        return self.reward_round2()
+
+
 class Arm2DEnv(OsimEnv):
     model_path = os.path.join(os.path.dirname(__file__), '../models/arm2dof6musc.osim')    
     time_limit = 200
