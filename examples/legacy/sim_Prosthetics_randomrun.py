62659b7b825c82406bd13a1fc5f841e870618cd8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-24 11:20:57 +0800

    Delete examples directory

--- examples/legacy/sim_Prosthetics_randomrun.py
+++
@@ -1,9 +0,0 @@
-from osim.env import ProstheticsEnv
-
-env = ProstheticsEnv(seed=5)
-env.change_model(model='3D', prosthetic=True, difficulty=2, seed=11)
-observation = env.reset()
-for i in range(300):
-    observation, reward, done, info = env.step(env.action_space.sample(), project = False)
-    if done:
-        env.reset(seed=10)
